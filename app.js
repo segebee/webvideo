ec8aa52c3cf9c1196529a3dacd41d0588c5552b6
Segun Abisagbo <segebee@gmail.com>
2018-01-26 08:40:40 +0100

    added canvas and picture snapping ability

--- app.js
+++ app.js
@@ -29,4 +29,31 @@ window.onload = function () {
     else{
         alert('your browser doesnt support this')
     }
+
+    var snap = document.getElementById('snap')
+
+    var video = window.video = document.getElementById("stream")
+    var canvas = window.canvas = document.getElementById("pictureContainer")
+    
+    canvas.width = 480
+    canvas.height = 360
+
+    snap.addEventListener("click", snapPicture, false)
+
+    function snapPicture()
+    {
+        //alert('i work')
+        var imageContainer = document.getElementById("imageContainer")
+        imageContainer.style.display = 'block'
+
+        canvas.getContext('2d').drawImage(video,0,0, canvas.width, canvas.height)
+        //updateImages(canvas.toDataURL());
+    }
+
+    function updateImages(src) {
+        var imgs = document.querySelectorAll(".demo__item img")
+        for (var i = 0; i < imgs.length; i++) imgs[i].src = src
+    }
+
+    
 }
